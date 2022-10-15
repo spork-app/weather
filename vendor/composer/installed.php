d11138c407015fb5c5e569ac764934b769e6400a
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-10-15 17:52:04 -0400

    wip

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'spork/weather',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f5aab5a7c0ede34b5bbca6453014cb3a24f0ec90',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'spork/weather' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f5aab5a7c0ede34b5bbca6453014cb3a24f0ec90',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
